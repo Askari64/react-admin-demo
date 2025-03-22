a41836a830f32b5d45ebda39b157988fb488e8e4
Askari <askarir64@gmail.com>
2025-03-22 17:51:26 +0530

    Dynamically Fetching Data
    Using useGetList Hook

--- ra-app/src/components/PosterComponents/PosterFilterList.tsx
+++ ra-app/src/components/PosterComponents/PosterFilterList.tsx
@@ -1,29 +1,35 @@
-import { FilterList, FilterListItem, FilterLiveSearch } from "react-admin";
-import {Card, CardContent} from '@mui/material'
-import CategoryIcon from "@mui/icons-material/LocalOffer"
-
+import {
+  FilterList,
+  FilterListItem,
+  FilterLiveSearch,
+  useGetList,
+} from "react-admin";
+import { Card, CardContent } from "@mui/material";
+import CategoryIcon from "@mui/icons-material/LocalOffer";
 
 function PosterFilterList() {
+  const { data, error } = useGetList("categories", {
+    sort: { field: "id", order: "ASC" },
+  });
+
+  if (error) return <p>{error.message}</p>;
+
   return (
-    <Card sx={{order: -1, mr: '24px', mt: '48px', width: 250}}>
-        <CardContent>
-            <FilterLiveSearch/>
-            <FilterList label="Categories" icon={<CategoryIcon/>}>
-            <FilterListItem label="animals" value={{category_id: 1}} />
-            <FilterListItem label="beard" value={{category_id: 2}} />
-            <FilterListItem label="business" value={{category_id: 3}} />
-            <FilterListItem label="cars" value={{category_id: 4}} />
-            <FilterListItem label="city" value={{category_id: 5}} />
-            <FilterListItem label="flowers" value={{category_id: 6}} />
-            <FilterListItem label="food" value={{category_id: 7}} />
-            <FilterListItem label="nature" value={{category_id: 8}} />
-            <FilterListItem label="people" value={{category_id: 9}} />
-            <FilterListItem label="sports" value={{category_id: 10}} />
-           
-            </FilterList>
-        </CardContent>
+    <Card sx={{ order: -1, mr: "24px", mt: "48px", width: 250 }}>
+      <CardContent>
+        <FilterLiveSearch />
+        <FilterList label="Categories" icon={<CategoryIcon />}>
+          {data?.map((category_data) => (
+            <FilterListItem
+              id={category_data.id}
+              label={category_data.name}
+              value={{ category_id: category_data.id }}
+            />
+          ))}
+        </FilterList>
+      </CardContent>
     </Card>
-  )
+  );
 }
 
-export default PosterFilterList
+export default PosterFilterList;
